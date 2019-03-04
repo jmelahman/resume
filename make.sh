e526a8cc730649c4a6e411c30a191b61723469d9
Jamison Lahman <lahmanja@protonmail.com>
2019-03-03 20:10:46 -0800

    add cover letter and bash script to make all pdfs

---
+++ make.sh
@@ -0,0 +1,4 @@
+ 
+pdflatex -synctex=1 -interaction=nonstopmode resume.tex
+pdflatex -synctex=1 -interaction=nonstopmode cover_letter.tex
+pdflatex -synctex=1 -interaction=nonstopmode cover_letter_resume.tex
